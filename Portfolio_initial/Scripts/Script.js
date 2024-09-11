b2dc0e65fbc6d461de21ecf6fa51f3e6c7d7a514
AniketDhakate007 <adhakate344@gmail.com>
2024-09-11 16:32:35 +0530

    1st Portfolio

---
+++ Portfolio_initial/Scripts/Script.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    // Basic dynamic projects list
+    const projects = ['TimeSaver Extension for Chrome'];
+
+    projects.forEach(project => {
+        $('#projects-list').append(`<li>${project}</li>`);
+    });
+});
